4df835dc48e1b7b6d35ae0275834060a70a5a380
ReAleksey <136960408+ReAleksey@users.noreply.github.com>
2023-07-12 14:59:44 +0500

    Add files via upload

---
+++ SkillboxM16.1/SkillboxM16.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkillboxM16.1
+//
+//  Created by Алексей Решетников on 12.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Coomit 2
+    }
+
+
+}
+
